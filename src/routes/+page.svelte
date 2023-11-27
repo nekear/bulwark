b3270d3107613c7ad88b644517e5a30a4bac9877
Nekear <diachenko_md@hotmail.com>
2023-11-27 16:27:44 +0200

    feat: added shadcn base configuration

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import {Button} from "$lib/components/ui/button";
+</script>
+
+<Button>Hello world</Button>
+
