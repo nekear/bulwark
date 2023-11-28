e21b271c0d6da743c516d52ac5b0b9ad14404b08
dart <dart78@ukr.net>
2023-11-28 11:03:39 +0200

    fix: fixed links highlight in the nav bar

--- src/routes/navigation.svelte
+++ src/routes/navigation.svelte
@@ -1,24 +1,37 @@
 <script lang="ts">
 	import '../app.pcss';
 	import { cn } from '$lib/utils';
+	import { page } from '$app/stores';
 
+	$: routeId = $page.route.id;
 	let className: string | undefined | null = undefined;
 	export { className as class };
 </script>
 
 <nav class={cn('flex items-center space-x-4 lg:space-x-6', className)}>
-	<a href="/" class="text-md font-medium transition-colors hover:text-primary"> Home </a>
+	<a
+		href="/"
+		class=" text-[{routeId === '/'
+			? '#FFFFFF'
+			: '#535353'}] font-medium transition-colors hover:text-primary"
+	>
+		Home
+	</a>
 
 	<a
 		href="/services"
-		class="text-md font-medium text-muted-foreground transition-colors hover:text-primary"
+		class=" text-[{routeId === '/services'
+			? '#FFFFFF'
+			: '#535353'}] font-medium transition-colors hover:text-primary"
 	>
 		Services
 	</a>
 
 	<a
 		href="/feedback"
-		class="text-md font-medium text-muted-foreground transition-colors hover:text-primary"
+		class="text-[{routeId === '/feedback'
+			? '#FFFFFF'
+			: '#535353'}] font-medium transition-colors hover:text-primary"
 	>
 		Feedback
 	</a>
